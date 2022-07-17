41fa6a0f8793925401a976dad1442996608ec977
peoft <paulo.ostermann.filho@gmail.com>
2022-07-17 13:36:15 -0300

    Criando um componente, usando bootstrap no layout e iterando no html - *ngFor

--- src/app/courses/course-list.component.ts
+++ src/app/courses/course-list.component.ts
@@ -1,4 +1,5 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { Course } from "./course";
 
 
 // Decorator descreve que a classe é um componente
@@ -7,6 +8,31 @@ import { Component } from "@angular/core";
     templateUrl: './course-list.component.html'
 })
 // Tornando a classe pública.
-export class CourseListComponent {
-    
+export class CourseListComponent implements OnInit{
+    courses: Course[] = [];
+
+    ngOnInit(): void {
+        this.courses = [
+            {
+                id: 1,
+                name: 'Angular: Forms',
+                imageUrl: '',
+                price: 99.99,
+                code: 'XPS-8796',
+                duration: 120,
+                rating: 5.4,
+                releaseDate: 'December, 2, 2019'
+            },
+            {
+                id: 2,
+                name: 'Angular: HTTP',
+                imageUrl: '',
+                price: 45.99,
+                code: 'LKL-3854',
+                duration: 30,
+                rating: 4,
+                releaseDate: 'November, 4, 2019'
+            },            
+        ]
+    }
 }
