9599598c815eae5f85e431969df7427136857d2b
peoft <paulo.ostermann.filho@gmail.com>
2022-07-17 12:22:52 -0300

    Definindo um componente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { CourseListComponent } from './courses/course-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CourseListComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/courses/course-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+
+// Decorator descreve que a classe é um componente
+@Component ({
+    selector: 'app-course-list',
+    template: '<h2> Course List </h2>'
+})
+// Tornando a classe pública.
+export class CourseListComponent {
+    
+}
