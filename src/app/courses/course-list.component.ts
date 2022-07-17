9c7331170222be7a74f1a358a42e7d10648a2729
peoft <paulo.ostermann.filho@gmail.com>
2022-07-17 12:29:04 -0300

    Usando o template url

--- src/app/courses/course-list.component.ts
+++ src/app/courses/course-list.component.ts
@@ -4,7 +4,7 @@ import { Component } from "@angular/core";
 // Decorator descreve que a classe é um componente
 @Component ({
     selector: 'app-course-list',
-    template: '<h2> Course List </h2>'
+    templateUrl: './course-list.component.html'
 })
 // Tornando a classe pública.
 export class CourseListComponent {
