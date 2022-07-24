f7338f863ffa9eacbe8eb5ec748ad2b7175717e9
peoft <paulo.ostermann.filho@gmail.com>
2022-07-24 11:15:16 -0300

    Segregando o componente estrela.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 // Módulo raiz
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
--- src/app/courses/course.module.ts
+++ src/app/courses/course.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 import { RouterModule } from "@angular/router";
 import { ReplacePipe } from "../pipe/replace.pipe";
-import { StarComponent } from "../star/star.component";
+import { StarModule } from "../shared/component/star/star.module";
 import { CourseInfoComponent } from "./course-info.component";
 import { CourseListComponent } from "./course-list.component";
 
@@ -11,12 +11,12 @@ import { CourseListComponent } from "./course-list.component";
     declarations: [
         CourseListComponent,
         CourseInfoComponent,
-        ReplacePipe,
-        StarComponent
+        ReplacePipe
     ],
     imports: [
         CommonModule,
         FormsModule,
+        StarModule,
         RouterModule.forChild([
             {
                 path: 'courses', component: CourseListComponent
